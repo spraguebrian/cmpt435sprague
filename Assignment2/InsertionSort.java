8d2533e5edf40d723058711a0df0f6e3c8482b7f
spraguebrian <47014386+spraguebrian@users.noreply.github.com>
2020-09-28 21:53:54 -0400

    Final Version of InsertionSort
    This one actually worked on my first try so I only need the 1 commit for it :)

---
+++ Assignment2/InsertionSort.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class InsertionSort {
+
+	public static void main(String[] args) throws IOException {
+		ArrayList<String> list = new ArrayList<>(Files.readAllLines(Paths.get("C:\\Users\\sprag\\eclipse-workspace\\Assignment2\\src\\magicitems.txt")));
+		//Copied from SelectionSort because that was the first one I did
+		/*
+		for(int z =0; z < list.size(); z++) {
+			System.out.println(list.get(z));
+		}
+		This is just a print function to make sure the file was read properly
+		*/
+		
+		int comparisons = 0;
+		
+		for(int i=0; i<list.size(); i++) {
+			int j=i;
+			while(j > 0 && list.get(j).compareToIgnoreCase(list.get(j-1)) < 0) {
+				//In the while loop you are checking to see if the value is greater than the one prevous to it
+				//So we are checking to see if "j" is greater than "j-1"
+				String temp = list.get(j);
+				list.set(j, list.get(j-1));
+				list.set(j-1, temp);
+				//Using a temp to switch values in the list
+				comparisons++;
+				j--;
+				//Need to decrease j to continue testing the previous values
+				//The while loop will break if j reaches 0, which is the start of the list
+			}
+		}
+		for(int y =0; y < list.size(); y++) {
+			System.out.println(list.get(y));
+		} //Printing out the sorted list
+		System.out.println("Number of Comparisons: " + comparisons);
+		
+	}
+
+}
