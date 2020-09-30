92647eeb1153f2d75c5235375929fc546467bbe4
spraguebrian <47014386+spraguebrian@users.noreply.github.com>
2020-09-29 20:42:56 -0400

    Update to MergeSort
    I have located the problem. It happens when I call the merge() while in sort()
    Changes in this commit will show the various print statements added while I was testing where the problem arises
    Going to keep working on a fix

--- Assignment2/MergeSort.java
+++ Assignment2/MergeSort.java
@@ -23,15 +23,29 @@ public class MergeSort {
 		System.out.println("Number of Comparisons: " + comparisons);
 	}
 	
-	void sort(ArrayList<String> list, int left, int right) { //When calling this function you use your array, 0(the left value), and list.size() (the right value)
+	void sort(ArrayList<String> list, int left, int right) { 
+		//When calling this function you use your array, 0(the left value), and list.size() (the right value)
+		/*
+		for(int z =0; z < list.size(); z++) {
+			System.out.println(list.get(z));
+		}
+		//Testing if list is read in
+		*/
+		//System.out.println("sort: Left: " + left);
+		//System.out.println("sort: Right: " + right);
+		//I had a problem with indexing 0 so these print statements
+		//are used to check where it went wrong
 		if(left < right) {
 			int middle = right / 2;
 			//This is used to find the midpoint, it will be rounded if the length is odd
+			//System.out.println("sort: middle: " + middle);
 			
 			sort(list, left, middle);
 			sort(list, middle+1, right);
 			//Need to continue splitting until the arrays are size 1, middle+1 is needed so we don't repeat values
-			
+			//System.out.println("sort: Left: " + left);
+			//System.out.println("sort: Right: " + right);
+			//System.out.println("sort: middle: " + middle);
 			merge(list, left, middle, right);
 		}
 	}
@@ -39,8 +53,13 @@ public class MergeSort {
 	void merge(ArrayList<String> list, int left, int middle, int right) {
 		int size1 = middle - left + 1;
 		int size2 = right-middle;
-		//Finding sizes of both arrays
 		
+		//Finding sizes of both arrays
+		System.out.println("merge: Left: " + left);
+		System.out.println("merge: Middle: " + middle);
+		System.out.println("merge: Right: " + right);
+		System.out.println("merge: Size 1: " + size1);
+		System.out.println("merge: Size 2: " + size2);
 		ArrayList<String> leftArr = new ArrayList<String>();
 		ArrayList<String> rightArr = new ArrayList<String>();
 		//Creating the temp arrays
